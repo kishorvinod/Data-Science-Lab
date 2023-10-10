aad8d101ae32c4cdd2471cf874881fc7acb6ec7b
Kishor Vinod <117641547+kishorvinod@users.noreply.github.com>
2023-10-10 14:43:38 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+print("*************************************************")
+print("SJC22MCA-2035 : KISHOR VINOD")
+print("MCA 2022-24")
+print("*************************************************")
+three_dim_array = np.array([
+    [
+        [1.0, 2.0, 3.0],
+        [4.0, 5.0, 6.0],
+    ],
+    [
+        [7.0, 8.0, 9.0],
+        [10.0, 11.0, 12.0],
+    ]
+], dtype=float)
+
+# Print the 3D array
+print(three_dim_array)
